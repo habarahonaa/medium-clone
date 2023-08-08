656ebcd5111f8a9900ecdcfa4af3489fdd16d217
Hollman A. Barahona <habarahonaa@gmail.com>
2023-08-08 10:59:14 -0600

    feat: swagger set-up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Medium Clone API')
+    .setDescription('The Medium Clone API built on NestJS')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
