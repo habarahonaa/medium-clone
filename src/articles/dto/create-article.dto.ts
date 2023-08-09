955e8a12647dfe4b525c876a51298e8b319993d7
Hollman A. Barahona <habarahonaa@gmail.com>
2023-08-09 16:25:27 -0600

    feat: add validationpipe to handle post/patch requests

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,15 +1,34 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateArticleDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   title: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  @MaxLength(300)
   @ApiProperty()
   description?: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   body: string;
 
-  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  @ApiProperty({ required: false, default: false })
   published?: boolean = false;
 }
