0249053d2f84fecc2e946f5fc8cc44adf2d07051
Hollman A. Barahona <habarahonaa@gmail.com>
2023-08-10 10:42:11 -0600

    feat: register passport and jwt module for auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+export const jwtSecret = 'zjP9h6ZI5LoSKCRj';
 
 @Module({
+  imports: [
+    PrismaModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtSecret,
+      signOptions: { expiresIn: '5m' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
